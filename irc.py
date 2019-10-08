06c00c9c85090646162ff2d492118962978b49f2
Jordan <jormit2000@gmail.com>
2019-10-08 20:20:44 +1100

    Add files via upload

--- irc.py
+++ irc.py
@@ -3,17 +3,19 @@ import time
 import threading
 import os
 from datetime import datetime
+import sys
 
 # Class handles all irc i/o.
 class Irc:
 	socket = socket.socket()
 
-	def __init__(self,  username, password, channel, server = "irc.freenode.net", port = 6667):
+	def __init__(self,  username, password, server = "irc.freenode.net", port = 6667):
 		self.username = username
 		self.server = server
-		self.port = port
-		self.channel = channel
+		self.port = int(port)
+		self.channel = ""
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.password = password
 
 	def connect(self):
 		print("Connecting to: " + self.server)
@@ -21,10 +23,10 @@ class Irc:
 
 		self.socket.send(bytes("USER " + self.username + " " + self.username +" " + self.username + " :python\n", "UTF-8"))
 		self.socket.send(bytes("NICK " + self.username + "\n", "UTF-8"))
-		#self.socket.send(bytes("NICKSERV IDENTIFY " + self.password + " " + self.password + "\n", "UTF-8"))
+		self.socket.send(bytes("NICKSERV IDENTIFY " + self.username + " " + self.password + "\n", "UTF-8"))
 		time.sleep(5)
 
-		self.socket.send(bytes("JOIN " + self.channel + "\n", "UTF-8"))
+		#self.socket.send(bytes("JOIN " + self.channel + "\n", "UTF-8"))
 
 	def response(self):		
 		time.sleep(1)
@@ -44,20 +46,27 @@ class Irc:
 			except:
 				print(resp)
 
-
 	def send(self,message):
 		irc.socket.send(bytes("PRIVMSG " + self.channel + " :"  + message + "\n", "UTF-8"))
 	
 	def handle_command(self, message):
 		if (message.startswith("/join")):
-			self.channel = message.split()[1]
-			self.socket.send(bytes("JOIN " + self.channel + "\n", "UTF-8"))
+			try:
+				self.channel = message.split()[1]
+				self.socket.send(bytes("JOIN " + self.channel + "\n", "UTF-8"))
+			except:
+				pass
 		if (message.startswith("/q")):
 			os._exit(1)
+		if (message.startswith("/names")):
+			try:
+				self.socket.send(bytes("NAMES " + self.channel + "\n", "UTF-8"))
+			except:
+				pass
 
 def get_input(irc):
 	while True:
-		message = input()			
+		message = input()
 
 		if (message.startswith("/")):
 			irc.handle_command(message)
@@ -65,9 +74,17 @@ def get_input(irc):
 
 		irc.send(message)
 		print(datetime.now().strftime('%d %H:%M:%S') + " <" + irc.username + "> " + message)
-		
 
-irc = Irc("zz", "password", "#main")
+
+if (len(sys.argv) < 3):
+	print("You need to specify the server to connect to:")
+	print("python main.py server.net 3333")
+	exit(1)
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+irc = Irc(username, password, sys.argv[1], sys.argv[2])
 
 irc.connect()
 
